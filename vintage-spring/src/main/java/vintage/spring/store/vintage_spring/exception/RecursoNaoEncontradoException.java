a7f43f113c3472b804863d55c2ed365774d1803c
AnnaLuiza-M <lulumoreira150@gmail.com>
2025-08-09 11:10:54 -0300

    Delete vintage-spring directory

--- vintage-spring/src/main/java/vintage/spring/store/vintage_spring/exception/RecursoNaoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package vintage.spring.store.vintage_spring.exception;
-
-public class RecursoNaoEncontradoException extends RuntimeException{
-    public RecursoNaoEncontradoException(String mensagem) {
-        super(mensagem);
-    }
-
-}
--- vintage-spring/src/main/java/vintage/spring/store/vintage_spring/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package vintage.spring.store.vintage_spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vintage.spring.store.vintage_spring.entities.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
